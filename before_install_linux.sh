68e8b14bc31d3c2d2cb051636a66b284dbbca1ff
Ilya Vassilevsky <vassilevsky@gmail.com>
2015-05-12 19:02:15 +0300

    Don't dump huge file list in test output

--- before_install_linux.sh
+++ before_install_linux.sh
@@ -8,12 +8,12 @@ sudo apt-get build-dep -y imagemagick
 case $IMAGEMAGICK_VERSION in
     latest)
         wget http://www.imagemagick.org/download/ImageMagick.tar.gz
-        tar -xzvf ImageMagick.tar.gz
+        tar -xzf ImageMagick.tar.gz
         cd ImageMagick-*
     ;;
     *)
         wget http://www.imagemagick.org/download/releases/ImageMagick-${IMAGEMAGICK_VERSION}.tar.gz
-        tar -xzvf ImageMagick-${IMAGEMAGICK_VERSION}.tar.gz
+        tar -xzf ImageMagick-${IMAGEMAGICK_VERSION}.tar.gz
         cd ImageMagick-${IMAGEMAGICK_VERSION}
     ;;
 esac
